48a94b9db49bc0fd4aae55920263c672da9640cd
Oleksandr Lebedko <lebedko.alexandr@gmail.com>
2019-05-17 13:29:05 +0300

    Change line brakes to LF

--- init-db.sh
+++ init-db.sh
@@ -7,3 +7,5 @@ psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
    GRANT ALL PRIVILEGES ON DATABASE alfresco_dev TO pivotal;
    GRANT ALL PRIVILEGES ON DATABASE cof_dev TO pivotal; 
 EOSQL
+
+
--- ux-start-up.sh
+++ ux-start-up.sh
@@ -2,4 +2,4 @@
 
 docker build --tag cra2-psql .
 
-docker run --name cra2-psql-container -p 5432:5432 -v postgres-data:/var/lib/postgresql/data cra2-psql
+docker run --name cra2-psql-container -p 5432:5432 -v postgres-data:/var/lib/postgresql/data cra2-psql 
